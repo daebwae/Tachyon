fba93fcadc6cc2a7a00f633582006e79a12917f3
Stefan Zapf <stefan@nerdocracy.org>
2015-02-28 09:19:07 +0100

    First check-in

---
+++ Tachyon/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tachyon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Brave new Tacyhon world");
+        }
+    }
+}
